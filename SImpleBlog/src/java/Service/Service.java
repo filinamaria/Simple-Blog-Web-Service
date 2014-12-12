1c553006aa62274f5ef6029fc10de1d8cd75174d
MarioFilino <mario.filino@gmail.com>
2014-12-12 18:09:23 +0700

    web service edited

--- SImpleBlog/src/java/Service/Service.java
+++ SImpleBlog/src/java/Service/Service.java
@@ -6,9 +6,12 @@
 
 package Service;
 
-import javax.jws.WebService;
+import com.firebase.client.*;
+import java.util.HashMap;
+import java.util.Map;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebService;
 
 /**
  *
@@ -18,10 +21,116 @@ import javax.jws.WebParam;
 public class Service {
 
     /**
-     * This is a sample web service operation
+     * addPost web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
+    @WebMethod(operationName = "addPost")
+    public boolean addPost(@WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") String tanggal, @WebParam(name = "author") String author) {
+        Firebase postReference = new Firebase("https://simpleblog5.firebaseio.com/post");
+        Map<String, String> post = new HashMap<String, String>();
+        
+        post.put("judul", judul);
+        post.put("konten", konten);
+        post.put("tanggal", tanggal);
+        post.put("author", author);
+        post.put("status", "unpublished");
+        
+        postReference.push().setValue(post);
+        return true;
+    }
+    
+    /**
+     * listPost web service operation
+     */
+    @WebMethod(operationName = "listPost")
+    public String listPost(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * editPost web service operation
+     */
+    @WebMethod(operationName = "editPost")
+    public String editPost(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * deletePost web service operation
+     */
+    @WebMethod(operationName = "deletePost")
+    public String deletePost(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * publishPost web service operation
+     */
+    @WebMethod(operationName = "publishPost")
+    public String publishPost(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * addUser web service operation
+     */
+    @WebMethod(operationName = "addUser")
+    public String addUser(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * listUser web service operation
+     */
+    @WebMethod(operationName = "listUser")
+    public String listUser(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * editUser web service operation
+     */
+    @WebMethod(operationName = "editUser")
+    public String editUser(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * deleteUser web service operation
+     */
+    @WebMethod(operationName = "deleteUser")
+    public String deleteUser(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * addComment web service operation
+     */
+    @WebMethod(operationName = "addComment")
+    public String addComment(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * listComment web service operation
+     */
+    @WebMethod(operationName = "listComment")
+    public String listComment(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * deleteComment web service operation
+     */
+    @WebMethod(operationName = "deleteComment")
+    public String deleteComment(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * search web service operation
+     */
+    @WebMethod(operationName = "search")
+    public String search(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
     }
 }
